0c3e790a7a13a734734c71d3e7c6942ecabbdb64
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2017-12-01 15:10:48 -0500

    Update lhashdos.lua

--- lhashdos.lua
+++ lhashdos.lua
@@ -7,8 +7,10 @@ local char_array
 
 do
 	math.randomseed(os.time())
+	
 	char_array = ffi.new 'char[15]'
 	local lowest_char = math.random((' o'):byte(1, 2))
+	
 	for i = lowest_char, lowest_char + 15 do
 		char_array[i - lowest_char] = i
 	end
@@ -19,14 +21,14 @@ end
 local function get(spacer, outputs_desired, restart_position)
 	local bit_rshift = bit.rshift
 	local start = restart_position or 1
-	local buf = ffi.new('collidable[?]', outputs_desired)
-	ffi.fill(buf, ffi.sizeof(buf), spacer:byte())
+	local buffer = ffi.new('collidable[?]', outputs_desired)
+	ffi.fill(buffer, ffi.sizeof(buffer), spacer:byte())
 
 	for buf_pos = 0, outputs_desired - start do
 		for offset = 0, 30, 2 do
-			buf[buf_pos][offset] = char_array[bit_rshift(buf_pos + start, offset) % 15]
+			buffer[buf_pos][offset] = char_array[bit_rshift(buf_pos + start, offset) % 15]
 		end
 	end
 
-	return buf
+	return buffer
 end
