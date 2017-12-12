0d2388a7e37a5f5668942781211a7aa0936bd60a
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2017-12-11 21:17:48 -0500

    Commented
    Hopefully looks less arcane.

--- c_example.c
+++ c_example.c
@@ -2,30 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define str_s sizeof(str)
-#define ignored 'v'
+// Settings
+const int  amt = 50000; // strings to generate
+const char ign = 'v';   // character to base strings off of 
 
+// Typedefs
 typedef char str[33];
 
-const char *symbols = "!\"#$%&'()*+,-./";
+// Algorithm to fill a buffer
 void fill(const int amt, str *buf) { 
-  for (int i = amt; i; i--)
-    for (int j = 0; j < 31; j += 2)
-      buf[i][j] = symbols[(i >> j) % 15];
+  static const char *symbols = "!\"#$%&'()*+,-./"; // Picked randomly. Nothing special about these.
 
-  for (int i = amt; i; i--)
-    buf[i][32] = '\0';
+  memset(buf, ign, amt * sizeof(str)); // Fill the buffer with junk that doesn't affect the hash
+  for (int i = amt; i; i--) // Generates amt strings
+    for (int j = 0; j < 31; j += 2) // Fills the character slots in those strings
+      buf[i][j] = symbols[(i >> j) % 15]; // Magic
+
+  for (int i = amt; i; i--) // Terminate all strs
+    buf[i][32] = '\0'; // Adds the terminator
 }
 
+// Creates buffer, calls fill on it, and prints results. 
 int main(int argc, char **argv) {
-  const long amt = 50000; // strings to generate
-  str *buffer = (str*) malloc(str_s * amt);
+  str *buffer = (str*) malloc(sizeof(str) * amt); // Allocate buffer
 
-  memset(buffer, ignored, amt * str_s);
-  fill(amt, buffer);
+  fill(amt, buffer); // Fill buffer
 
-  for(int i = 0; i < amt; i++)
-    puts(buffer[i]);
+  for(int i = 0; i < amt; i++) // Print buffer contents
+    puts(buffer[i]); // Call to puts for printing
   
   return 0;
 }
