384d158bb5623df08e9052702b02b2cbe0c8a4d0
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2017-12-11 21:04:22 -0500

    Update c_example.c
    Cleaned up.

--- c_example.c
+++ c_example.c
@@ -1,21 +1,31 @@
-// obfuscated for dumb reasons, will probably upload version with better names eventually
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef char p[33];
-const char *s="!\"#$%&'()*+,-./";
-void f(const int t,p*o) { 
-  for(int i=t;i;i--)for(int j=0;j<31;j+=2)o[i][j]=s[(i>>j)%15];
-  for(int i=t;i;i--)o[i][32]='\0';
+#define str_s sizeof(str)
+#define ignored 'v'
+
+typedef char str[33];
+const char   *symbols = "!\"#$%&'()*+,-./";
+
+void fill(const int amt, str *buf) { 
+  for (int i = amt; i; i--)
+    for (int j = 0; j < 31; j += 2)
+      buf[i][j] = symbols[(i >> j) % 15];
+
+  for (int i = amt; i; i--)
+    buf[i][32] = '\0';
 }
 
-int main(int argc,char**argv) {
-  const long t=1337;
-  p*b=(p*)malloc(sizeof(p)*t);
-  memset(b,'v',t*sizeof(p));
-  f(t,b);
-  for(int i=0;i<t;i++)puts(b[i]);
+int main(int argc, char **argv) {
+  const long amt = 50000; // strings to generate
+  str *buffer = (str*) malloc(str_s * amt);
+
+  memset(buffer, ignored, amt * str_s); // where v = 
+  fill(amt, buffer);
+
+  for(int i = 0; i < amt; i++)
+    puts(buffer[i]);
+  
   return 0;
 }
